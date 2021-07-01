f298efd43b97d390613ca98ad7b472f061a5363a
Sharjeel Moqrab Khan <38564835+SharjeelMoqrabKhan@users.noreply.github.com>
2021-07-01 13:12:54 +0500

    added sankbar

--- lib/app/screens/home.dart
+++ lib/app/screens/home.dart
@@ -35,51 +35,58 @@ class _HomeState extends State<Home> {
           _loading ? 'Loading...' : 'Hashlob-Products',
         ),
       ),
-      body: Column(
-        children: [
-          TextField(
-            decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(10),
-              hintText: "Search",
-            ),
-            onChanged: (value) {
-              setState(() {
-                _filterProducts = _product.where((element) {
-                  return element.name
-                          .toLowerCase()
-                          .contains(value.toLowerCase()) ||
-                      element.price.contains(value);
-                }).toList();
-              });
-            },
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                Datum product = _filterProducts[index];
-                return ListTile(
-                  title: Text(
-                    product.name,
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                  ),
-                  subtitle: Text("Add to cart"),
-                  trailing: Text(
-                    'Price: ${product.price}',
-                    style: TextStyle(
-                        color: Colors.redAccent,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  onTap: () {
-                    widget._valueSetter(_filterProducts[index]);
-                  },
-                );
+      body: Builder(builder: (context) {
+        return Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(10),
+                hintText: "Search",
+              ),
+              onChanged: (value) {
+                setState(() {
+                  _filterProducts = _product.where((element) {
+                    return element.name
+                            .toLowerCase()
+                            .contains(value.toLowerCase()) ||
+                        element.price.contains(value);
+                  }).toList();
+                });
               },
-              itemCount: null == _filterProducts ? 0 : _filterProducts.length,
             ),
-          ),
-        ],
-      ),
+            Expanded(
+              child: ListView.builder(
+                itemBuilder: (context, index) {
+                  Datum product = _filterProducts[index];
+                  return ListTile(
+                    title: Text(
+                      product.name,
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("Add to cart"),
+                    trailing: Text(
+                      'Price: ${product.price}',
+                      style: TextStyle(
+                          color: Colors.redAccent,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    onTap: () {
+                      widget._valueSetter(_filterProducts[index]);
+                      SnackBar cartSnakBar = SnackBar(
+                        content: Text("added ${product.name}"),
+                      );
+                      Scaffold.of(context).showSnackBar(cartSnakBar);
+                    },
+                  );
+                },
+                itemCount: null == _filterProducts ? 0 : _filterProducts.length,
+              ),
+            ),
+          ],
+        );
+      }),
     );
   }
 }
